a3c49ba7ebef59dc2664f5997c79504be4b3fc68
xxh <tang2988@qq.com>
2019-04-24 02:50:22 +0800

    添加登陆注册页面

--- src/main/java/com/shangcheng/controller/CustomerController.java
+++ src/main/java/com/shangcheng/controller/CustomerController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class CustomerController {
 	
-	@RequestMapping("/index")
+	@RequestMapping("/login")
 	public String index() {
-		return "index";
+		return "login";
 	}
 
 }
